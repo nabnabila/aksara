83957cea64ce3f67cdf80dcb8d593cd2b99cfb09
nabnabila <nabila.indhy@gmail.com>
2024-09-02 01:29:39 +0700

    finishing detail

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,13 +3,13 @@ import { Container, Row, Col } from "react-bootstrap";
 const Intro = () => {
   return (
     <div className="intro">
-      <Container className="d-flex justify-content-center align-items-center">
+      <Container className="d-flex justify-content-left align-items-left">
         <Row>
           <Col>
-            <div className="title">Ayo Belajar Carakan</div>
-            <div className="note">
-              Kenalan sama aksara Jawa yang terlihat indah ini yuk!
-            </div>
+            <div className="title">Ayo Belajar</div>
+            <div className="title1">Carakan</div>
+            <div className="note">Kenalan sama aksara Jawa</div>
+            <div className="note">yang terlihat indah ini yuk!</div>
           </Col>
         </Row>
       </Container>
